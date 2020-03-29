66cf848f98660d27e8c288ac92afaee976ad4d2f
oddminng <oddminng@gmail.com>
2020-03-29 22:59:51 +0800

    10. Using Channels Done!

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -4,8 +4,14 @@ defmodule Rumbl.Accounts do
   The Accounts context.
   """
 
+  import Ecto.Query
+
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
+
+  def list_users_with_ids(ids) do
+    Repo.all(from(u in User, where: u.id in ^ids))
+  end
 
   def get_user(id) do
     Repo.get(User, id)
